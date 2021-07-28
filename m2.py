bcfc696d578d2112e3dfbdc3d2e5debcd58a084f
DevHackingS2 <87339066+DevHackingS2@users.noreply.github.com>
2021-07-28 00:48:03 -0300

    Add files via upload

--- m2.py
+++ m2.py
@@ -1,22 +1,34 @@
 import os
+import sys
+import os
 import time
-import tkinter
-from tkinter import *
-from typing import Sized
+import colorama
+from colorama import Fore, Back, Style
 
-root = Tk()
+os.system("clear")
 
-class Application():    
+nome = input(f"{Style.DIM}Digite seu Nome: ")
 
-    def __init__(self):
-        self.root   = root
-        self.tela()
-        root.mainloop()
-    def tela(self):
-        self.root.title("===== by: Namoradinho! ===== ")
-        self.root.configure(background='black')
-        self.root.geometry("700x700")
+ascii = """
 
+/***
+ *    01010111 01001000  01000100 01010000 01010010 
+ */
+                                        =! Feito Pelo Seu Namoradinho !=
+"""
 
+menu = """ =! [Bem vindo {} !] !=
+ =! Regras !=
+ =! [1] >-> !=
+ =! [2] Não tem regras, apenas divirta-se lendo meu textinho ! !=
+""".format(nome)
 
-Application()
+for char in ascii:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.03)
+
+for char in menu:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.1)
